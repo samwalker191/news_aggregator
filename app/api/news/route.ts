7f64bc76827f3c80a3f65ccf077f16fb823376b3
Sam Walker <sewalker191@gmail.com>
2024-11-16 11:52:55 -0800

    Add post endpoint for articles

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,6 +1,7 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/db";
 import { Article } from "@prisma/client";
+import { zfd } from "zod-form-data";
 
 export async function GET(request: NextRequest): Promise<NextResponse<Article[]>>  {
     const searchParams = request.nextUrl.searchParams;
@@ -19,4 +20,37 @@ export async function GET(request: NextRequest): Promise<NextResponse<Article[]>
     })
 
     return NextResponse.json(result);
+}
+
+const schema = zfd.formData({
+    title: zfd.text(),
+    description: zfd.text(),
+    state: zfd.text(),
+    topic: zfd.text(),
+    author: zfd.text(),
+    source: zfd.text(),
+    url: zfd.text(),
+    imageUrl: zfd.text(),
+    content: zfd.text(),
+    publishedDate: zfd.text()
+})
+
+export async function POST(request: Request): Promise<NextResponse<Article | string | unknown>> {
+    const formData = await request.formData();
+    let articleData;
+    try {
+        articleData = schema.parse(formData);
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
+    
+    let article: Article;
+    try {
+        article = await prisma.article.create({
+            data: articleData
+        })
+    } catch (error) {
+        return NextResponse.json("Something went wrong in saving the article, please try again.", { status: 400 });
+    }
+    return NextResponse.json(article);
 }
